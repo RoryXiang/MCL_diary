b5194999f2b8265622b0ad8a0e929bbc7cb22dc6
Rory_work_windos <494423520@qq.com>
2018-12-13 14:34:29 +0800

    add choose beast feature function

--- learn_decision_tree.py
+++ learn_decision_tree.py
@@ -62,14 +62,36 @@ def split_data(data, axis, value):
 
 
 def choose_bast_feature_to_split(data):
+    """
+    找到理想特征。思想是，针对每一特征计算他们的特征信息熵，取信息熵最大者作为最优特征
+    :param data:
+    :return:
+    """
     feature_num = len(data[0])
-    bast_entropy = clacShannonEnt(data)  # 计算信息熵
+    base_entropy = clacShannonEnt(data)  # 计算信息熵
     beast_info_gain = 0.0
     beast_feature = -1
     for i in range(feature_num-1):
+        # 创建唯一的分类标签列表-------------------------
         feature_list = [example[i] for example in data]
         unique_values = set(feature_list)
         new_entropy = 0.0
+        # 计算第i个特征每个特征值的信息熵----------------
         for value in unique_values:
             sub_data = split_data(data, i, value)
             prob = len(sub_data)/float(len(data))
+            new_entropy += prob*clacShannonEnt(sub_data)  # TODO
+        info_gain = base_entropy - new_entropy
+        # print("&&&&&", info_gain)
+        # 如果此信息的信息熵比之前的高，则代替之前的-----
+        if info_gain > beast_info_gain:
+            beast_info_gain = info_gain
+            beast_feature = i
+    return beast_feature
+
+
+data, _ = create_data()
+feature = choose_bast_feature_to_split(data)
+print(feature)
+
+
