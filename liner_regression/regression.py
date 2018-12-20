421013397634a1ecb9270115c63d958a1d0240de
向尚平 <xiangshangping@wtoip.com>
2018-12-20 17:42:37 +0800

    complete regression

--- liner_regression/regression.py
+++ liner_regression/regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def load_data(filename):
@@ -16,7 +17,13 @@ def load_data(filename):
     return data_mat, label_mat
 
 
+# 标准线性回归
 def stand_regres(xarr, yarr):
+    """
+    :param xarr:  特征值
+    :param yarr: 目标值
+    :return:
+    """
     x_mat = np.mat(xarr)
     y_mat = np.mat(yarr).T
     xTx = x_mat.T*x_mat
@@ -24,21 +31,22 @@ def stand_regres(xarr, yarr):
         print("This is singular, cant not do inverse")
         return
     ws = np.linalg.solve(xTx, x_mat.T*y_mat)
+    # ws = xTx.I * x_mat.T * y_mat
     return ws
 
 
 xarr, yarr = load_data("ex0.txt")
-print(xarr[0:2])
+# print(xarr[0:2])
 
 ws = stand_regres(xarr, yarr)
-print(ws)
+# print(ws)
 
 x_mat = np.mat(xarr)
 y_mat = np.mat(yarr)
 y_hat = x_mat*ws
 
-import matplotlib.pyplot as plt
-
+# 把数据和预测直线画出来-------------------------------------------------------
+#
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.scatter(x_mat[:,1].flatten().A[0], y_mat.T[:, 0].flatten().A[0])  # numpy 的操作
@@ -47,3 +55,48 @@ x_copy.sort(0)
 y_hat = x_copy*ws
 ax.plot(x_copy[:,1], y_hat)
 plt.show()
+
+# -----------------------------------------------------------------------------
+
+
+# 局部加权线性回归  k值
+# 增加了核函数，使用高斯核函数，相当于只用于当前数据点相近的部分数据计算回归系数
+def lwlr(test_point, xarr, yarr, k=1.0):
+    x_mat = np.mat(xarr)
+    y_mat = np.mat(yarr).T
+    m = np.shape(x_mat)[0]
+    weigths = np.mat(np.eye((m)))  # 创建对角矩阵
+    for j in range(m):
+        diff_mat = test_point - x_mat[j]
+        weigths[j, j] = np.exp(diff_mat*diff_mat.T/(-2.0*k**2))  # 权重值大小以指级衰减
+    xTx = x_mat.T * (weigths*x_mat)
+    if np.linalg.det(xTx) == 0:
+        print("This is singular, cant not do inverse")
+        return
+    ws = xTx.I * x_mat.T * weigths * y_mat  # TODO ????
+    return test_point * ws
+
+print(yarr[0])
+
+
+def lwlr_test(testarr, xarr, yarr, k=1.0):
+    m = np.shape(testarr)[0]
+    y_hat = np.zeros(m)
+    for i in range(m):
+        y_hat[i] = lwlr(testarr[i], xarr, yarr, k)
+    return y_hat
+
+
+#
+y_hat = lwlr_test(xarr, xarr, yarr, 0.01)
+x_mat = np.mat(xarr)
+srt_ind = x_mat[:, 1].argsort(0)
+x_sort = x_mat[srt_ind][:, 0, :]
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(x_sort[:, 1], y_hat[srt_ind])
+ax.scatter(x_mat[:, 1].flatten().A[0], np.mat(yarr).T.flatten().A[0], s=2, c="red")
+plt.show()
+
+print(np.mat(np.array([1,2,3]))*np.mat(np.array([1,2,3])).T)
+print(np.sum(np.mat(np.array([1,2,3]))*np.mat(np.array([1,2,3])).T))
