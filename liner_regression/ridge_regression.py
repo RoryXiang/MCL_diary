a75f2452e9bdc82e0d98dcce175520d77d61447f
向尚平 <xiangshangping@wtoip.com>
2018-12-24 11:02:31 +0800

    add redge regression file

--- liner_regression/ridge_regression.py
+++ liner_regression/ridge_regression.py
@@ -1,6 +1,9 @@
 import numpy as np
-from liner_regression.regression import load_data
+from liner_regression.regression import load_data, stand_regres
+import matplotlib.pyplot as plt
 
+
+# 岭回归----------------------------------------------------
 def rigre_regression(x_mat, y_mat, lam=0.2):
     xTx = x_mat.T*x_mat
     denom = xTx + np.eye(np.shape(x_mat)[1]) * lam
@@ -22,10 +25,85 @@ def ridge_test(xarr,yarr):
     numTestPts = 30
     w_mat = np.zeros((numTestPts, np.shape(x_mat)[1]))
     for i in range(numTestPts):
-        ws = np.ridgeRegres(x_mat, y_mat, np.exp(i-10))
+        ws = rigre_regression(x_mat, y_mat, np.exp(i-10))
         w_mat[i] = ws.T
     return w_mat
 
 
-abx, aby = load_data("./abalone.txt")
-w_ = ridge_test(abx, aby)
+def regularize(x_mat):#regularize by columns
+    in_mat = x_mat.copy()
+    in_means = np.mean(in_mat, 0)   #calc mean then subtract it off
+    in_var = np.var(in_mat, 0)      #calc variance of Xi then divide by it
+    in_mat = (in_mat - in_means)/in_var
+    return in_mat
+
+
+def plot():
+    abx, aby = load_data("./abalone.txt")
+    w_ = ridge_test(abx, aby)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # 展示结果-------------------------------------
+    # x_mat = np.mat(abx)
+    # y_hat = w_ * x_mat
+    # ax.scatter(x_mat[:, 1].flatten().A[0], y_hat.T[:, 0].flatten().A[0])
+    # x_copy = x_mat.copy()
+    # x_copy.sort(0)
+    # ax.plot(x_copy[:, 1], y_hat)
+    # ----------------------------------------------
+
+    ax.plot(w_)
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot()
+
+
+# 计算平方误差
+def rss_error(yarr, yhat):
+    return ((yarr-yhat)**2).sum()
+
+
+# 前向逐步回归--------------------------------------------------------------
+def stage_wise(xarr, yarr, eps=0.01, num_it=100):
+    x_mat = np.mat(xarr)
+    y_mat = np.mat(yarr).T
+    y_mean = np.mean(y_mat, 0)
+    y_mat = y_mat - y_mean
+    x_mat = (x_mat - np.mean(x_mat, 0)) / np.var(x_mat, 0)
+    # x_mat = regularize(x_mat)
+    m, n = np.shape(x_mat)
+    return_mat = np.zeros((num_it, n))
+    ws = np.zeros((n, 1))
+    ws_test = ws_max = ws.copy()
+    for i in range(num_it):
+        lowest_error = np.inf
+        for j in range(n):
+            # 每个特征值的权重，尝试增加或减小一定的数值来查看平方误差
+            for sign in [-1, 1]:
+                ws_test = ws.copy()
+                ws_test[j] += eps*sign
+                y_test = x_mat *ws_test
+                rss_e = rss_error(y_mat.A, y_test.A)
+                if rss_e < lowest_error:
+                    ws_max = ws_test
+        ws = ws_max.copy()
+    return return_mat
+
+
+def show__():
+    xarr, yarr = load_data("./abalone.txt")
+    a = stage_wise(xarr, yarr, 0.01, 5000)
+    x_mat = np.mat(xarr)
+    y_mat = np.mat(yarr).T
+    x_mat = regularize(x_mat)
+    y_mean = np.mean(y_mat, 0)
+    y_mat = y_mat - y_mean
+    weights = stand_regres(x_mat, y_mat.T)
+    print(weights.T)
+
+
+if __name__ == '__main__':
+    show__()
--- predict_salse/predict.py
+++ predict_salse/predict.py
@@ -9,10 +9,17 @@ from liner_regression.regression import stand_regres, lwlr
 from predict_salse.standrd_data import get_stanred_x_y_arr, get_origin_x_y_arr
 
 
-def pridict_by_liner_regression(xarr, yarr, xtest):
+def predict_by_liner_regression(xarr, yarr, xtest):
     ws = stand_regres(xarr, yarr)
     c = np.sum(xtest * ws)
     return c
 
 
+if __name__ == '__main__':
+    xtest = [30, 38, 166806]
+    xarr, yarr = get_origin_x_y_arr()
+    stand_x, stnd_y = get_stanred_x_y_arr()
+    salse = predict_by_liner_regression(xarr, yarr, xtest)
+    stand_salse = predict_by_liner_regression(stand_x, stnd_y, xtest)
+    print(salse, stand_salse)
 
